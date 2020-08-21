dc4cd8272d817d45fdf9167df1b0dc8314e3bf69
shenghh <shenghh2015@gmail.com>
2020-08-21 16:10:39 -0500

    test

--- v100/multi_GPU.sh
+++ v100/multi_GPU.sh
@@ -1,5 +1,5 @@
-JOB: python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_1984_1024 --CUT_mode CUT --load_size 1984 --crop_size 1024 --batch_size 1 --gpu_ids 0 --n_epochs 1 --display_id 0
-JOB: python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_1984_1024 --CUT_mode CUT --load_size 1984 --crop_size 1024 --batch_size 1 --gpu_ids 1 --n_epochs 1 --display_id 0
-JOB: python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 2 --gpu_ids 2 --n_epochs 1 --display_id 0
-JOB: python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 2 --gpu_ids 3 --n_epochs 1 --display_id 0
+JOB: python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_1984_1024 --CUT_mode CUT --load_size 1984 --crop_size 1024 --batch_size 1 --gpu_ids 0 --n_epochs 1 --display_id 0
+JOB: python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_1984_1024 --CUT_mode CUT --load_size 1984 --crop_size 1024 --batch_size 1 --gpu_ids 1 --n_epochs 1 --display_id 0
+JOB: python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 2 --gpu_ids 2 --n_epochs 1 --display_id 0
+JOB: python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 2 --gpu_ids 3 --n_epochs 1 --display_id 0
 
