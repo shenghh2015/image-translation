c22cd95d120a75cae953b2acdf687d0381717338
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-22 15:17:29 -0500

    test

--- v100/job_folder/job_0.sh
+++ v100/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_1984_1024 --CUT_mode CUT --load_size 1984 --crop_size 1024 --batch_size 1 --gpu_ids 0 --n_epochs 1 --display_id 0
+python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_1984_800 --CUT_mode CUT --load_size 1984 --crop_size 800 --batch_size 1 --gpu_ids 0 --n_epochs 1 --display_id 0
 
--- v100/job_folder/job_3.sh
+++ v100/job_folder/job_3.sh
@@ -1,2 +1,2 @@
-python ../train.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 2 --gpu_ids 3 --n_epochs 1 --display_id 0
+python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 1 --gpu_ids 3 --n_epochs 1 --display_id 0
 
