e0b8ee501486d3337e997a3e3b409aa8d453df9f
shenghh <shenghh2015@gmail.com>
2020-08-22 15:07:14 -0500

    test

--- v100/multi_submit.sh
+++ v100/multi_submit.sh
@@ -3,4 +3,4 @@ export LSF_DOCKER_VOLUMES='/scratch1/fs1/anastasio/Data_FDA_Breast/image_transla
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_IPC=host 
 export LSF_DOCKER_SHM_SIZE=40G
-bsub -G compute-anastasio -n 4 -R 'span[ptile=4] select[mem>200000] rusage[mem=200GB]' -q anastasio -a 'docker(shenghh2020/tf_cut_py3.5:1.0)' -gpu "num=4" -o /scratch1/fs1/anastasio/Data_FDA_Breast/image_translation/logs/4GPU_$RANDOM /bin/bash /home/shenghuahe/image_translation/trans/v100/multi_loader.sh
+bsub -G compute-anastasio -n 4 -R 'span[ptile=4] select[mem>200000] rusage[mem=200GB]' -q general -a 'docker(shenghh2020/tf_cut_py3.5:1.0)' -gpu "num=4" -o /scratch1/fs1/anastasio/Data_FDA_Breast/image_translation/logs/4GPU_$RANDOM /bin/bash /home/shenghuahe/image_translation/trans/v100/multi_loader.sh
