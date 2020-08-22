d31833dd47164636520f6a2d621fb2d6ada89f99
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-22 15:32:31 -0500

    test

--- v100/job_folder/job_3.sh
+++ v100/job_folder/job_3.sh
@@ -1,2 +1,1 @@
-python /data/trans/train.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 800 --batch_size 1 --gpu_ids 3 --n_epochs 1 --display_id 0
-
+python /data/trans/test.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --results_dir /data/data/data/results/ --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 992 --phase test --batch_size 1 --gpu_ids 3 --n_epochs 1 --display_id 0
