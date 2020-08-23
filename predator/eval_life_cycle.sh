ba2f0b95e295f61eef74aa53c915113d9c09509d
shenghh <shenghh2015@gmail.com>
2020-08-22 22:18:55 -0500

    test

---
+++ predator/eval_life_cycle.sh
@@ -0,0 +1,1 @@
+python ../test.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --results_dir ../../data/results/ --name life_dead_992_800 --CUT_mode CUT --phase test --load_size 992 --crop_size 512 --epoch latest  --gpu_ids -1
