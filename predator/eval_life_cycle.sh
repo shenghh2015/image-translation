e271f2c6cbc40430010e809751c80b42ee33513e
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-22 23:07:50 -0500

    test

---
+++ predator/eval_life_cycle.sh
@@ -0,0 +1,1 @@
+python ../test.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --results_dir ../../data/results/ --name life_dead_992_800 --CUT_mode CUT --phase test --load_size 416 --crop_size 416 --epoch latest  --gpu_ids -1
