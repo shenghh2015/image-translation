5f9ff757e18e708cbe5192221ca3a77c02fe00e0
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-24 12:11:30 -0500

    test

--- v100/job_folder/job_2.sh
+++
@@ -1,2 +0,0 @@
-python /data/trans/test.py --dataroot /data/data/datasets/life_cycle --checkpoints_dir /data/data/checkpoints --results_dir /data/data/results/ --name life_dead_992_800 --CUT_mode CUT --load_size 992 --crop_size 992 --phase test --batch_size 1 --gpu_ids 2 --epoch latest
-
