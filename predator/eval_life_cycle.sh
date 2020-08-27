bd89ba48a65e897912c482291f7430bca55c0a91
shenghh <shenghh2015@gmail.com>
2020-08-27 10:25:59 -0500

    test

--- predator/eval_life_cycle.sh
+++ predator/eval_life_cycle.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-python ../test.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --results_dir ../../data/results/ --name life_dead_992_800 --CUT_mode CUT --phase test --load_size 416 --crop_size 416 --epoch latest  --gpu_ids -1
-=======
-python ../test.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --results_dir ../../data/results/ --name life_dead_992_800 --CUT_mode CUT --phase test --load_size 992 --crop_size 512 --epoch latest  --gpu_ids -1
->>>>>>> ba2f0b95e295f61eef74aa53c915113d9c09509d
+python ../test.py --dataroot ../../data/datasets/life_cycle --checkpoints_dir ../../data/checkpoints --results_dir ../../data/results/ --name life_dead_496_400 --CUT_mode CUT --phase test --load_size 496 --crop_size 496 --epoch latest  --gpu_ids 1
